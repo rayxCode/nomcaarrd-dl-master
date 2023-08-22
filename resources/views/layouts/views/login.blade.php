e694773b7e21e36166bfdf121c0681fe04a40bbb
rayxCode <130419224+rayxCode@users.noreply.github.com>
2023-08-22 22:02:21 +0800

    Second update

---
+++ resources/views/layouts/views/login.blade.php
@@ -0,0 +1,12 @@
+@section('content')
+<div class="mx-auto md-flex">
+<div style="width: 500; height 720">
+    <img src="" alt="" srcset="">
+
+<form action="" method="post">
+
+</form>
+</div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing.index');
 });
+Route::get('/login', function () {
+    return view('layouts.views.login');
+});
 
