c915ad6feda4a9c38de3e4b7e8596afc609e2f5b
rayxCode <130419224+rayxCode@users.noreply.github.com>
2023-11-28 21:31:34 +0800

    Update admin_users.blade.php

--- resources/views/admin/admin_users.blade.php
+++ resources/views/admin/admin_users.blade.php
@@ -12,6 +12,54 @@
 
 @section('admin-layouts')
     <!-- Content Wrapper. Contains page content -->
+    <div class="modal mx-auto" id="modal">
+        <div class="modal-content p-3">
+            <form action="" method="post">
+                <div class="d-flex">
+                    <div style="width: 50%;">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" placeholder="Username" class="form-control" />
+                        <br />
+                        <label for="firstname" class="form-label">Firstname</label>
+                        <input type="text" placeholder="firstname" class="form-control" />
+                        <br />
+                        <label for="middlename" class="form-label">Middlename</label>
+                        <input type="text" placeholder="middlename" class="form-control" />
+                        <br />
+                        <label for="lastname" class="form-label">Lastname</label>
+                        <input type="text" placeholder="lastname" class="form-control" />
+                    </div>
+
+                    <div class="v1 ms-2"></div>
+
+                    <div class="ms-2" style="width: 50%;">
+                        <label for="affiliation" class="form-label">Affiliation</label>
+                        <select id="affiliation" name="affiliation" class="form-select" va>
+                            <option value="1">This is number 1</option>
+                            <option value="2">This is number 2</option>
+                            <option value="3">This is number 3</option>
+                            <option value="4">This is number 4</option>
+                        </select>
+                        <br>
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" required />
+                        <label for="cfpassword" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" required />
+                    </div>
+                </div>
+
+                <div class="modal-footer mt-3 mx-auto">
+                    <button class="modal-close rounded-pill btn btn-secondary" onclick="closeModal()" style="width: 150px;">
+                        Cancel
+                    </button>
+                    <button class="ms-2 modal-button rounded-pill btn btn-success" onclick="closeModal()"
+                        style="width: 150px;">
+                        Save
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
@@ -88,5 +136,13 @@
                 console.log(data);
                 // You can pass the data to your controller using AJAX or any suitable method
             });
-        </scrip
+
+            function onClickListenerBtn() {
+                document.getElementById("modal").style.display = "block";
+            }
+
+            function closeModal() {
+                document.getElementById("modal").style.display = "none";
+            }
+        </script>
     @endsection
